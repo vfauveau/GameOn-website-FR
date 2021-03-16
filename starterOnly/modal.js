6864b4a09d7115d1953b8372bacb75dcfa3675ad
vfauveau <76257448+vfauveau@users.noreply.github.com>
2021-03-16 18:33:49 +0100

    messages fonctionnels - la vérification de la naissance
    date de naissance restante à vérifier.
    + message erreur a ajouter
    code a rendre plus propre (fonctions)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,22 +17,24 @@ const form = document.getElementById('form');
 const submit = document.querySelector('input[type=submit]');
 
 // messages d'erreurs  à implémenter dans chaque span d'erreur
-const firstErreur = "Le prénom doit contenir au moins 2 caractères";
-const lastErreur = "le nom doit contenir au moins 2 caractères";
-const emailErreur = "Veuillez entrer une adresse email valide";
-const birthdateErreur = "Veuillez entrer une date de naissance valide";
-const quantityErreur = "Veuillez indiquer votre nombre de participation";
-const villesErreur = "Veuillez choisir une ville de participation";
-const checkboxErreur = "Cette case est obligatoire";
+const messages = [
+  "Le prénom doit contenir au moins 2 caractères.",
+  "Le nom doit contenir au moins 2 caractères.",
+  "Veuillez entrer une adresse email valide.",
+  "Veuillez entrer une date de naissance valide.",
+  "Veuillez indiquer votre nombre de participation.",
+  "Veuillez choisir une ville de participation.",
+  "Veuillez accepter les conditions d'utilisations."
+]
 
 // creation du message erreur
 var messageErreur = document.createElement("span");
 messageErreur.classList.add('erreur');
-messageErreur.style.display="none";
-
-function erreurMessage(erreur,endroit){
+messageErreur.style.display = "none";
+// fonction qui append le message d'erreur
+function erreurMessage(erreur, endroit) {
   messageErreur.textContent = erreur;
-  messageErreur.style.display="inline";
+  messageErreur.style.display = "inline";
   endroit.parentNode.appendChild(messageErreur);
 }
 
@@ -60,62 +62,70 @@ function validate() {
   var quantity = document.querySelector("input[name='quantity']");
   var radios = document.querySelectorAll("input[type=radio][name=location]");
   var checkboxReq = document.getElementById('checkbox1');
-  var validation = true ;
-  messageErreur.style.display="none"; // reset les messages d'erreurs
+  var validation = true;
+  messageErreur.style.display = "none"; // reset les messages d'erreurs
 
   /* test du prénom */
   if (firstName.value.length < 2 || firstName.value == "") {
     console.log("le prenom doit contenir au moins 2 caracteres");
     firstName.focus();
-    erreurMessage(firstErreur,firstName);
-    validation = false ;
+    erreurMessage(messages[0], firstName);
+    validation = false;
   }
 
   /* test du nom de famille */
   else if (lastName.value.length < 2 || lastName.value == "") {
     console.log("le nom doit contenir au moins 2 caracteres");
-    lastName.focus()
-    erreurMessage(lastErreur,lastName);
-    validation = false ;
+    lastName.focus();
+    erreurMessage(messages[1], lastName);
+    validation = false;
   }
 
   /* test du mail */
   if (!email.value.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i)) {
     console.log(" Veuillez entrer une adresse mail valide");
     email.focus()
-    erreurMessage(emailErreur,email);
-    validation = false ;
+    erreurMessage(messages[2], email);
+    validation = false;
   }
 
   /* test que le nombre de fois spécifié est bien un nombre / entre 0 et 99 */
   if (quantity.value == "" || quantity.value > 99) {
     console.log("le nombre doit etre compris entre 0 et 99");
     quantity.focus()
-    erreurMessage(quantityErreur,quantity);
-    validation = false ;
+    erreurMessage(messages[4], quantity);
+    validation = false;
   }
 
   // boucle pour vérifier si chaque bouton radio est coché, si oui renvoie true
   var valeur;
-  for(var i = 0; i < radios.length; i++){
-   if(radios[i].checked){
-   valeur = radios[i].value;
-   }
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      valeur = radios[i].value;
+      if (valeur != undefined) {
+        break;
+      }
+    }
   }
 
-  // si la boucle ne trouve rien renvoi false
-  if (valeur === undefined){console.log("Veuillez choisir un lieu");
-
-  validation = false ;
-}
-
-  // checkbox required
-  if (checkboxReq.checked === false)
-    {console.log('il faut cocher la case !!!!');
-    erreurMessage(checkboxErreur,checkboxReq);
-    validation = false ;
+  // si la boucle ne trouve rien renvoi false et affiche un message d'erreur
+  if (valeur === undefined) {
+    console.log("Veuillez choisir un lieu");
+    messageErreur.textContent = messages[5];
+    messageErreur.style.display = "inline";
+    formData[6].insertAdjacentElement('beforebegin', messageErreur);
+    validation = false;
   }
 
-  if (validation = false){};
-  return false
+  // checkbox required vérification que la case est cochée sinon affiche un message d'erreur
+  if (checkboxReq.checked === false) {
+    console.log('il faut cocher la case !!!!');
+    messageErreur.textContent = messages[6];
+    messageErreur.style.display = "inline";
+    formData[6].insertAdjacentElement('beforebegin', messageErreur);
+    validation = false;
+  }
+
+  if (validation === false) { return false };
+
 }
